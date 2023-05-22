1b4b50882efbbc2043d742d2d06733b10115fbeb
konta <konta8240@g.ecc.u-tokyo.ac.jp>
2023-05-22 11:09:35 +0900

    commit_5_22_2

--- Documents/hackathon/slack-like-app/slack-like-react/src/App.tsx
+++ Documents/hackathon/slack-like-app/slack-like-react/src/App.tsx
@@ -8,6 +8,7 @@ import { fireAuth } from "./firebase";
 import SignUp from "./components/SignUp"
 import DefaultPage from "./DefaultPage"
 import SignIn from './components/SignIn';
+import logo from './logo.svg';
 
 
 
@@ -56,12 +57,27 @@ function App() {
   }
 
 
-  
+
 
 
   return (
     <div>
-      <p>aaa</p>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
       <div>
         
         {currentPage === "defaultPage" && pageContent}
