05c7ab5be85769449ce66fead693056ef0e344de
konta <konta8240@g.ecc.u-tokyo.ac.jp>
2023-05-25 00:16:14 +0900

    commit_5_24

--- Documents/hackathon/slack-like-app/dao/channel_dao/channel_dao.go
+++ Documents/hackathon/slack-like-app/dao/channel_dao/channel_dao.go
@@ -2,15 +2,40 @@ package channel_dao
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/oklog/ulid/v2"
 	"log"
 	"math/rand"
+	"os"
 	"slack-like-app/model"
 	"time"
 )
 
 var db *sql.DB
+
+func init() {
+	// ①-1 環境変数を用いる
+
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	_db, err := sql.Open("mysql", connStr)
+
+	// ①-2
+	//_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlPwd, mysqlDatabase))
+	if err != nil {
+		log.Fatalf("fail: sql.Open, %v\n", err)
+	}
+	// ①-3
+	if err := _db.Ping(); err != nil {
+		log.Fatalf("fail: _db.Ping, %v\n", err)
+	}
+	db = _db
+}
 
 func RegisterChannel(channel_data model.ChannelReqForPost) (model.ChannelResForPost, error) {
 	t := time.Now()
@@ -31,11 +56,11 @@ func RegisterChannel(channel_data model.ChannelReqForPost) (model.ChannelResForP
 	}
 	log.Println("ok user table")
 
-	log.Println(id, channel_data.ChannelName,channel_data.WorkspaceId, t)
+	log.Println(id, channel_data.ChannelName, channel_data.WorkspaceId, t)
 
 	if err := tx.Commit(); err != nil {
 		return model.ChannelResForPost{}, err
 	}
 
-	return model.ChannelResForPost{ChannelId: id, ChannelName : channel_data.ChannelName, WorkspaceId: channel_data.WorkspaceId, RegisteredAt: t}, nil
+	return model.ChannelResForPost{ChannelId: id, ChannelName: channel_data.ChannelName, WorkspaceId: channel_data.WorkspaceId, RegisteredAt: t}, nil
 }
--- Documents/hackathon/slack-like-app/dao/workspace_dao/workspace_dao.go
+++ Documents/hackathon/slack-like-app/dao/workspace_dao/workspace_dao.go
@@ -2,15 +2,40 @@ package worksapce_dao
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/oklog/ulid/v2"
 	"log"
 	"math/rand"
+	"os"
 	"slack-like-app/model"
 	"time"
 )
 
 var db *sql.DB
+
+func init() {
+	// ①-1 環境変数を用いる
+
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	_db, err := sql.Open("mysql", connStr)
+
+	// ①-2
+	//_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlPwd, mysqlDatabase))
+	if err != nil {
+		log.Fatalf("fail: sql.Open, %v\n", err)
+	}
+	// ①-3
+	if err := _db.Ping(); err != nil {
+		log.Fatalf("fail: _db.Ping, %v\n", err)
+	}
+	db = _db
+}
 
 func RegisterWorkspace(workspace_data model.WorkspaceReqForPost) (model.WorkspaceResForPost, error) {
 	t := time.Now()
@@ -37,5 +62,5 @@ func RegisterWorkspace(workspace_data model.WorkspaceReqForPost) (model.Workspac
 		return model.WorkspaceResForPost{}, err
 	}
 
-	return model.WorkspaceResForPost{WorkspaceId: id, WorkspaceName : workspace_data.WorkspaceName, RegisteredAt: t}, nil
+	return model.WorkspaceResForPost{WorkspaceId: id, WorkspaceName: workspace_data.WorkspaceName, RegisteredAt: t}, nil
 }
