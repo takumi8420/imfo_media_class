1417c5dbbd90ad7bcf3d00503631136523aeba58
konta <konta8240@g.ecc.u-tokyo.ac.jp>
2023-06-04 17:35:43 +0900

    commit_6_4

--- Documents/hackathon/slack-like-app/controller/messages_controller/send_messsages_controller.go
+++ Documents/hackathon/slack-like-app/controller/messages_controller/send_messsages_controller.go
@@ -11,7 +11,7 @@ import (
 
 func SendMessagesHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	if r.Method == "OPTIONS" {
